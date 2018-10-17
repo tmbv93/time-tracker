69326f220645fa808da54c7087e8481bb798aad6
Tomas <tmbv93@gmail.com>
2018-10-17 11:50:47 +0200

    Made action noti

--- app/webpack/models/action_notice.js
+++ app/webpack/models/action_notice.js
@@ -1,5 +1,24 @@
 export default class ActionNotice {
     static success(text) {
-        console.log(text)
+        this.setType('success')
+        this.setText(text)
+        setTimeout(this.reset, 1000)
+    }
+
+
+    static reset() {
+        ActionNotice.setType('initial-state')
+        ActionNotice.setText('')
+    }
+
+    static setText(text) {
+        let html_text_element = document.getElementById('action-notice-text')
+        html_text_element.innerHTML = text
+    }
+
+    static setType(type) {
+        let html_element = document.getElementById('action-notice')
+        html_element.className = 'action-notice'
+        html_element.classList.add(type)
     }
 }
